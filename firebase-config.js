c3570fa21f0fac603c2292d6ea8999a861ae7535
sstextilessdv-design <sstextilessdv@gmail.com>
2026-08-30 15:18:32 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -9,12 +9,12 @@
 // Firestore/Storage Rules provide the real security.
 
 window.FIREBASE_CONFIG = {
-  apiKey: "PASTE_YOUR_FIREBASE_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
-  appId: "PASTE_YOUR_FIREBASE_APP_ID"
+  apiKey: "firebase init",
+  authDomain: "firebase init.firebaseapp.com",
+  projectId: "firebase init",
+  storageBucket: "firebase init.firebasestorage.app",
+  messagingSenderId: "firebase init",
+  appId: ""
 };
 
 window.STORE_ACCESS = {
