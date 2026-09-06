60e48c1e58aab71077ec5ccef863bcad9e35ff04
sstextilessdv-design <sstextilessdv@gmail.com>
2026-09-06 11:18:02 +0530

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,1 +1,1 @@
-
+import ImageKit from imagekit const imagekit = new ImageKit { publicKey: process.env.IMAGEKIT_PUBLIC_KEY, privateKey: process.env.IMAGEKIT_PRIVATE_KEY, urlEndpoint: process .env.IMAGEKIT_URL_ENDPOINT, } export default async function handler(req, res) [ // Get authenres.status (200).json(result);tication parametersconst result = imagekit.getAuthenticationParameters();`
